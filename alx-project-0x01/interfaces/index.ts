71b8fb26bfd4ebf0013d793ce3f7596be9e97ee4
Solomon Aberra Melesse <134952231+SolomonA17@users.noreply.github.com>
2025-07-17 10:35:39 +0300

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -5,37 +5,35 @@ export interface PostProps {
   body: string;
 }
 
-
 export interface Welcome {
-    id:       number;
-    name:     string;
-    username: string;
-    email:    string;
-    address:  Address;
-    phone:    string;
-    website:  string;
-    company:  Company;
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
 }
 
 export interface Address {
-    street:  string;
-    suite:   string;
-    city:    string;
-    zipcode: string;
-    geo:     Geo;
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
 }
 
 export interface Geo {
-    lat: string;
-    lng: string;
+  lat: string;
+  lng: string;
 }
 
 export interface Company {
-    name:        string;
-    catchPhrase: string;
-    bs:          string;
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
-
 
 export interface PostData {
   userId: number;
@@ -48,7 +46,6 @@ export interface PostModalProps {
   onClose: () => void;
   onSubmit: (post: PostData) => void;
 }
-
 
 export interface UserModalProps {
   isOpen: boolean;
@@ -78,6 +75,7 @@ export interface UserProps {
     bs: string;
   };
 }
+
 export interface UserData {
   name: string;
   username: string;
